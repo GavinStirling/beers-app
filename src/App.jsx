90bb7dcf7fea10a85d52f47c6ab61b9c460c78db
Gavin Stirling <gavinstir@gmail.com>
2022-06-14 09:30:13 +0100

    Still fixing search and filter

--- src/App.jsx
+++ src/App.jsx
@@ -20,24 +20,33 @@ function App() {
     const search = (event) => {
         const term = event.target.value;
         console.log(term);
-        if (term !== "") {
-            // handleSearchInput();
+        if (term != "") {
+            setActiveSearch(true);
             const results = beers.filter((beer) => {
-                return beer.name.toLowerCase().includes(term);
+                return beer.name.toLowerCase().includes(term.toLowerCase());
             });
             console.log(activeSearch, results);
             return results;
-        } else if (term === "") {
+        } else {
             console.log(activeSearch);
+            setActiveSearch(false);
             return beers;
         }
+    };
+
+    const HomeJSX = (array) => {
+        return (
+            <>
+                <Home array={array} />
+            </>
+        );
     };
 
     return (
         <div className="App">
             <Nav search={search} />
-            {!activeSearch && <Home array={beers} />}
-            {activeSearch && <Home array={search} />}
+            {/* {activeSearch ? HomeJSX(search()) : HomeJSX(beers)} */}
+            {/* {activeSearch && <Home array={search()} />} */}
             {/* <Home array={beers} /> */}
             {/* <Home array={search} /> */}
         </div>
