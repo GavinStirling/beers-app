98a956ad61cf43110cfc7829199ce0f251a62c98
Gavin Stirling <gavinstir@gmail.com>
2022-06-14 13:28:26 +0100

    Connected to PunkAPI and fixed search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Nav from "./containers/Nav/Nav";
 import Home from "./containers/Home/Home";
@@ -7,47 +7,65 @@ import Home from "./containers/Home/Home";
 import beers from "./data/beers";
 
 function App() {
-    const [activeSearch, setActiveSearch] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
 
-    const handleSearchInput = () => {
-        if (!activeSearch) {
-            setActiveSearch(true);
-        } else {
-            setActiveSearch(false);
+    const [array, setArray] = useState(beers);
+
+    const getBeers = async (searchTerm) => {
+        let url = "https://api.punkapi.com/v2/beers";
+
+        if (searchTerm != ""){
+            const temp = searchTerm.split(" ");
+            const cleanSearchTerm = temp.join("_")
+            url+= `?beer_name=${cleanSearchTerm.toLowerCase()}`
+        }
+
+        const res = await fetch(url);
+        const data = await res.json();
+        setArray(data);
+    };
+
+    const handleSearchInput = (event) => {
+        if (event.target.value != "") {
+            setSearchTerm(event.target.value);
         }
     };
 
-    const search = (event) => {
-        const term = event.target.value;
-        console.log(term);
-        if (term != "") {
-            setActiveSearch(true);
-            const results = beers.filter((beer) => {
-                return beer.name.toLowerCase().includes(term.toLowerCase());
-            });
-            console.log(activeSearch, results);
-            return results;
-        } else {
-            console.log(activeSearch);
-            setActiveSearch(false);
-            return beers;
-        }
-    };
+    // const search = (event) => {
+    //     const term = event.target.value;
+    //     console.log(term);
+    //     if (term != "") {
+    //         setActiveSearch(true);
+    //         const results = beers.filter((beer) => {
+    //             return beer.name.toLowerCase().includes(term.toLowerCase());
+    //         });
+    //         console.log(activeSearch, results);
+    //         return results;
+    //     } else {
+    //         console.log(activeSearch);
+    //         setActiveSearch(false);
+    //         return beers;
+    //     }
+    // };
 
-    const HomeJSX = (array) => {
-        return (
-            <>
-                <Home array={array} />
-            </>
-        );
-    };
+    // const HomeJSX = (array) => {
+    //     return (
+    //         <>
+    //             <Home array={array} />
+    //         </>
+    //     );
+    // };
+
+    useEffect(() => {
+        getBeers(searchTerm);
+    }, [searchTerm]);
 
     return (
         <div className="App">
-            <Nav search={search} />
+            <Nav search={handleSearchInput} />
             {/* {activeSearch ? HomeJSX(search()) : HomeJSX(beers)} */}
             {/* {activeSearch && <Home array={search()} />} */}
-            {/* <Home array={beers} /> */}
+            <Home array={array} />
             {/* <Home array={search} /> */}
         </div>
     );
