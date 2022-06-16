d3f2e2e5f8c120a3261b7f713fff9250d507053e
Gavin Stirling <gavinstir@gmail.com>
2022-06-16 09:22:06 +0100

    Started testing

---
+++ src/components/Card/Card.test.jsx
@@ -0,0 +1,13 @@
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+
+// import Card from "./Card";
+
+// describe("Basic tests to see if the card is working correctly", () => {
+//     it("Renders a card", () => {
+//         render(<Card />)
+
+        
+
+//     })
+// })
