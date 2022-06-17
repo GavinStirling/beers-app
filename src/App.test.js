6aea0c26e2f08c932c7ee769c4175db230560bd2
Gavin Stirling <gavinstir@gmail.com>
2022-06-17 10:05:55 +0100

    Updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('Renders containers', () => {
-  render(<App />);
+describe("Renders correctly to the page", () => {
+    it("Renders containers", () => {
+        render(<App />);
 
-  const Nav = screen.getByTestId("nav");
-  const Home = screen.getByTestId("home");
+        const Nav = screen.getByTestId("nav");
+        const Home = screen.getByTestId("home");
 
-  expect(Nav).toBeInTheDocument();
-  expect(Home).toBeInTheDocument();
+        expect(Nav).toBeInTheDocument();
+        expect(Home).toBeInTheDocument();
+    });
+
+    it("Renders all the cards from the API", () => {
+        render(<App />);
+
+        const cards = screen.getAllByTestId("card");
+        let noOfCards = 0;
+
+        cards.forEach((card) => {
+            noOfCards += 1;
+        });
+
+        expect(noOfCards).toBe(6);
+    });
 });
